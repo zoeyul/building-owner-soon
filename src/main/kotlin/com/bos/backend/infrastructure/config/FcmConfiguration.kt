95afecc390e94b7cbaa1ab351eb8a8d6df66ca85
khcdev <chakh9601@gmail.com>
2025-11-01 15:44:38 +0900

    feat: 테스트 실패 수정

--- src/main/kotlin/com/bos/backend/infrastructure/config/FcmConfiguration.kt
+++ src/main/kotlin/com/bos/backend/infrastructure/config/FcmConfiguration.kt
@@ -4,6 +4,7 @@ import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import java.io.ByteArrayInputStream
@@ -11,6 +12,12 @@ import java.io.FileInputStream
 import java.nio.charset.StandardCharsets
 
 @Configuration
+@ConditionalOnProperty(
+    prefix = "firebase",
+    name = ["enabled"],
+    havingValue = "true",
+    matchIfMissing = false,
+)
 class FcmConfiguration {
     @Value("\${firebase.credentials-path:}")
     private val credentialsPath: String = ""
